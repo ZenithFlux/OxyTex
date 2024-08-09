5eb30f210eb5c574c0bc755564bb73f88a07a8df
Chaitanya Lakhchaura <90706514+ZenithFlux@users.noreply.github.com>
2024-08-09 14:22:25 +0530

    added basic functions to textdetect

--- src/oxytex.cpp
+++ src/oxytex.cpp
@@ -1,7 +1,13 @@
+#include <iostream>
+#include <opencv2/opencv.hpp>
 #include <textdetect/detect.hpp>
+#include <tuple>
 
 
 int main() {
-    textdetect::load_models();
+    cv::Mat img = txdt::load_image("build/test_files/test.jpg");
+    double target_ratio;
+    std::tie(img, target_ratio) = txdt::resize_aspect_ratio(img, 1280, cv::INTER_LINEAR, 1.5);
+    img = txdt::normalize_mean_variance(img);
     return 0;
 }
--- src/textdetect/detect.hpp
+++ src/textdetect/detect.hpp
@@ -1,8 +1,26 @@
 #ifndef OXYTEX_TEXTDETECT_H_
 #define OXYTEX_TEXTDETECT_H_
 
-namespace textdetect {
-    void load_models();
+#include <string>
+#include <opencv2/opencv.hpp>
+
+namespace txdt {
+
+cv::Mat load_image(const std::string& img_file);
+
+std::pair<cv::Mat, double> resize_aspect_ratio(
+    cv::Mat img,
+    std::size_t square_size,
+    int interpolation,
+    double mag_ratio = 1
+);
+
+cv::Mat normalize_mean_variance(
+    cv::Mat img,
+    cv::Scalar mean = {0.485, 0.456, 0.406},
+    cv::Scalar variance = {0.229, 0.224, 0.225}
+);
+
 }
 
 #endif
